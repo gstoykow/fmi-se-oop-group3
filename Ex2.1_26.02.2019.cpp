2cd6e055f0ae557c76f91b4eb70b5ef61fb51f31
gstoykow <45628637+gstoykow@users.noreply.github.com>
2019-03-01 02:53:04 +0300

    Update Ex2.1_26.02.2019.cpp

--- Ex2.1_26.02.2019.cpp
+++ Ex2.1_26.02.2019.cpp
@@ -9,19 +9,22 @@ struct Beer
 
 	Beer()
 	{
-		this->name = "Default beer";
+		this->name = new char[30];
+		strcpy_s(this->name, 30, "Default");
 		this->price = 2;
 	}
 
 	Beer(char* name, float price)
 	{
-		this->name = name;
+		this->name = new char[30];
+		strcpy_s(this->name, 30, name);
 		this->price = price;
 	}
 
 	Beer(const Beer& b)
 	{
-		this->name = b.name;
+		this->name = new char[30];
+		strcpy_s(this->name, 30, b.name);
 		this->price = b.price;
 	}
 
@@ -41,25 +44,30 @@ struct User
 
 	User()
 	{
-		this->username = "Default";
+		this->username = new char[30];
+		strcpy_s(this->username, 30, "Default");
 		this->record_count = 0;
 		//No need for creating an instance of Beer. It is already created at (*)
 	}
 
 	User(char* username, int record_count,const Beer& b)
 	{
-		this->username = username;
+		this->username = new char[30];
+		strcpy_s(this->username, 30, username);
 		this->record_count = record_count;
 		//using the copy constructor
-		this->favourite = Beer(b);
+		//we will learn about the bottom line in future excercises
+		//this->favourite = Beer(b);
 	}
 
 	User(const User& u)
 	{
-		this->username = u.username;
+		this->username = new char[30];
+		strcpy_s(this->username, 30, u.username);
 		this->record_count = u.record_count;
 		//using the copy constructor
-		this->favourite = Beer(u.favourite);
+		//we will learn about the bottom line in future excercises
+		//this->favourite = Beer(u.favourite);
 	}
 
 	~User()
@@ -71,7 +79,10 @@ struct User
 
 int main()
 {
+	Beer b1;
+	Beer b2(b1);
 
+	User u1("gosho", 5, b1);
 
 	cin.get();
 
