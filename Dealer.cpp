b58398577261c388b1529aed3951f06e16d68fa2
gstoykow <45628637+gstoykow@users.noreply.github.com>
2019-03-12 15:27:47 +0200

    Update Dealer.cpp

--- Dealer.cpp
+++ Dealer.cpp
@@ -1,96 +1,108 @@
 #include "Dealer.h"
 
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
 Dealer::Dealer()
 {
+    //in excercise
 }
 
 Dealer::Dealer(char * name, Automobile ** automobiles)
 {
+    //in excercise
 }
 
 Dealer::Dealer(const Dealer & d)
 {
+    //in excercise
 }
 
 Dealer::~Dealer()
 {
+    //in excercise
 }
 
 void Dealer::setName(char * name)
 {
-	this->name = new char[strlen(name)+1];
-	strcpy_s(this->name, strlen(name)+1, name);
+    this->name = new char[strlen(name)+1];
+    strcpy_s(this->name, strlen(name)+1, name);
 }
 
 char * Dealer::getName() const
 {
-	return this->name;
+    return this->name;
 }
 
 void Dealer::addAutomobile(const Automobile & a)
 {
-	if (size + 1 >= getCap())
-	{
-		setCap(getCap()* 2);
-		Automobile** newAutomobiles = new Automobile*[getCap()];
-		for (int i = 0; i < getSize(); i++)
-		{
-			newAutomobiles[i] = automobiles[i];
-		}
-		newAutomobiles[size++] = new Automobile(a);
+    if (size + 1 >= getCap())
+    {
+        setCap(getCap()* 2);
+        Automobile** newAutomobiles = new Automobile*[getCap()];
+        for (int i = 0; i < getSize(); i++)
+        {
+            newAutomobiles[i] = automobiles[i];
+        }
+        newAutomobiles[size++] = new Automobile(a);
 
-		delete[] automobiles;
-		automobiles = newAutomobiles;
-	}
-	else 
-	{
-		automobiles[getSize()] = new Automobile(a);
-		setSize(getSize() + 1);
-	}
-	
+        delete[] automobiles;
+        automobiles = newAutomobiles;
+    }
+    else
+    {
+        automobiles[getSize()] = new Automobile(a);
+        setSize(getSize() + 1);
+    }
+
 
 }
 
 bool Dealer::removeAutomobile(int index)
 {
-	if (index < getSize())
-	{
-		delete automobiles[index];
+    if (index < getSize())
+    {
+        delete automobiles[index];
 
-		for (int i = index; i < getSize() - 1; i++)
-		{
-			automobiles[i] = automobiles[i + 1];
-		}
+        for (int i = index; i < getSize() - 1; i++)
+        {
+            automobiles[i] = automobiles[i + 1];
+        }
 
-		setSize(size - 1);
+        setSize(size - 1);
 
-		return true;
-	}
-	return false;
+        return true;
+    }
+    return false;
 }
 
 Automobile & Dealer::getAutomobile(int index)
 {
-	if (index < getSize())
-		return *automobiles[index];
-	else cout<<"Index is invalid!";
-	
+    if (index < getSize())
+        return *automobiles[index];
+    else cout<<"Index is invalid!";
+
 }
 
 void Dealer::setSize(int size)
 {
+    this->size = size;
 }
 
 int Dealer::getSize() const
 {
-	return 0;
+    return this->size;
 }
 
 void Dealer::setCap(int cap)
 {
+    if(cap > 0)
+        this->cap = cap;
 }
 
 int Dealer::getCap() const
 {
-	return 0;
+    return this->cap;
 }
