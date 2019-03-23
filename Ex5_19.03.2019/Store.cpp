c1b75dc7a93d93f767c7f6f365dc24a0e0a83883
gstoykow <45628637+gstoykow@users.noreply.github.com>
2019-03-23 18:56:23 +0300

    Create Store.cpp

---
+++ Ex5_19.03.2019/Store.cpp
@@ -0,0 +1,104 @@
+#include "Store.h"
+
+void Store::increaseSsize()
+{
+	this->ssize++;
+}
+
+void Store::decreaseSsize()
+{
+	if (getSsize() > 0)
+	this->ssize--;
+}
+
+void Store::increaseScap()
+{
+	if (this->scap == 0)
+	{
+		this->scap = 2;
+	}
+	else
+	{
+		this->scap *= 2;
+	}
+}
+
+int Store::getScap() const
+{
+	return this->scap;
+}
+
+void Store::resize()
+{
+	increaseScap();
+	Product** newProducts = new Product*[scap];
+
+	for (int i = 0; i < ssize; i++)
+	{
+		newProducts[i] = products[i];
+	}
+
+	delete[] products;
+	products = newProducts;
+}
+
+int Store::getSsize() const
+{
+	return this->ssize;
+}
+
+void Store::addProduct(const Product &p)
+{
+	if (ssize == scap)resize();
+
+	increaseSsize();
+
+	products[getSsize() - 1] =
+		new Product(p.getBrand(), p.getModel(), p.getPrice(), p.getCount());
+}
+
+bool Store::removeProduct(unsigned int index)
+{
+	if (index > getSsize())
+		return 0;
+	else
+	{
+		delete products[index];
+		for (int i = index; i < getSsize() - 1; i++)
+		{
+			products[i] = products[i + 1];
+		}
+		decreaseSsize();
+		return true;
+	}
+}
+
+bool Store::changeProduct(unsigned int index, const Product& p)
+{
+	if (index > getSsize())
+		return 0;
+	else
+	{
+		delete products[index];
+		products[index] =
+			new Product(p.getBrand(), p.getModel(), p.getPrice(), p.getCount());
+
+	}
+}
+
+void Store::printStore() const
+{
+	for (int i = 0; i < getSsize(); i++)
+	{
+		products[i]->print();
+	}
+}
+
+Store::~Store()
+{
+	for (int i = 0; i < getSsize(); i++)
+	{
+		delete products[i];
+	}
+	delete[] products;
+}
